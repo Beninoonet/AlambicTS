35856c982c18cf14f330903a1a34d6682993d575
beninoonet <beninoonet@pm.me>
2023-11-19 02:33:11 +0100

    VoiceUpdate for just my server (ephemeral)

--- src/commands/clear.ts
+++ src/commands/clear.ts
@@ -1,4 +1,6 @@
 import { Command } from '@sapphire/framework';
+import { EmbedBuilder, TextChannel } from 'discord.js';
+
 
 export class ClearCommand extends Command {
     public constructor(context: Command.Context, options: Command.Options) {
@@ -14,9 +16,30 @@ export class ClearCommand extends Command {
             builder
                 .setName('clear')
                 .setDescription("Permet d'effacer un nombre de messages précis ")
+                .addNumberOption(option => {
+                    return option
+                        .setName('nombre')
+                        .setRequired(true)
+                        .setDescription('Spécifié le nombre de message à supprimer')
+                        .setMinValue(1)
+                }),
         );
     }
 
     public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+
+        const amount = interaction.options.getNumber('nombre');
+
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: `${interaction.client.user.username}`, iconURL: `${interaction.client.user.displayAvatarURL()}` })
+            .setColor('Red')
+            .setTimestamp()
+
+        if (amount) {
+            (interaction.channel as TextChannel).bulkDelete(amount, true);
+            embed.setTitle(`${amount} message(s) supprimé(s)`)
+            embed.addFields({ name: `Commande effecuté par`, value: `Salon: ${interaction.user.displayName}` })
+        }
+
     }
 }
--- src/listeners/voiceStateUpdate.ts
+++ src/listeners/voiceStateUpdate.ts
@@ -15,26 +15,28 @@ export class VoiceStateListener extends Listener {
         const webhookVoice = new WebhookClient({ url: `${process.env.VOICE_WEBHOOK}` })
         let voiceEmbed = new EmbedBuilder()
             .setTimestamp()
-
+        let newUserGuild = newUser.guild.id
         let newUserChannel = newUser.channelId;
         let oldUserChannel = oldUser.channelId;
 
-        if (oldUserChannel == undefined && newUserChannel !== undefined) {
-            voiceEmbed.setTitle(`${newUser.member?.user.displayName} est en vocal.`)
-            voiceEmbed.addFields({
-                name: `Salon`,
-                value: `${newUser.channel}`
-            })
-            voiceEmbed.addFields({
-                name: `Activité du salon`,
-                value: `${newUser.channel?.members.size} Membre(s)`
-            })
-            voiceEmbed.setThumbnail(`${newUser.member?.displayAvatarURL({ size: 1024 })}`)
-            webhookVoice.send({ embeds: [voiceEmbed] })
-            this.container.logger.info(`${newUser.member?.user.username} a rejoint le salon : ${newUser.channel?.name} `)
-        }
-        else if (newUserChannel == undefined) {
-            this.container.logger.info(`${oldUser.member?.user.username} a quitté le salon : ${oldUser.channel?.name} `)
+        if (newUserGuild === process.env.GUILD_ID) {
+            if (oldUserChannel == undefined && newUserChannel !== undefined) {
+                voiceEmbed.setTitle(`${newUser.member?.user.displayName} est en vocal.`)
+                voiceEmbed.addFields({
+                    name: `Salon`,
+                    value: `${newUser.channel}`
+                })
+                voiceEmbed.addFields({
+                    name: `Activité du salon`,
+                    value: `${newUser.channel?.members.size} Membre(s)`
+                })
+                voiceEmbed.setThumbnail(`${newUser.member?.displayAvatarURL({ size: 1024 })}`)
+                webhookVoice.send({ embeds: [voiceEmbed] })
+                this.container.logger.info(`${newUser.member?.user.username} a rejoint le salon : ${newUser.channel?.name} | Serveur : ${oldUser.guild.name} `)
+            }
+            else if (newUserChannel == undefined) {
+                this.container.logger.info(`${oldUser.member?.user.username} a quitté le salon : ${oldUser.channel?.name} | Serveur : ${oldUser.guild.name} `)
+            }
         }
     }
 
