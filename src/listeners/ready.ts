91408f65bd0ed21c96b9d8b599866cb813ef2a4d
beninoonet <beninoonet@pm.me>
2023-11-19 03:38:01 +0100

    Design a Ready.ts

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,5 +1,9 @@
 import { Events, Listener } from '@sapphire/framework';
-import { ActivityType, Client } from 'discord.js';
+import { ActivityType, Client, EmbedBuilder, WebhookClient } from 'discord.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export class ReadyListener extends Listener {
     public constructor(context: Listener.Context, options: Listener.Options) {
         super(context, {
@@ -12,7 +16,42 @@ export class ReadyListener extends Listener {
     run(client: Client) {
         const { username, id } = client.user!;
         this.container.logger.info(`Successfully logged in as ${username} (${id})`);
-        client.user?.setActivity(`${client.guilds.cache.size} servers`, { type: ActivityType.Watching })
+        client.user?.setActivity(`Lov U`, { type: ActivityType.Watching })
+
+        const LogHook = new WebhookClient({ url: `${process.env.LOGS_WEBHOOK}` })
+        const Logs = new EmbedBuilder()
+            .setColor('Green')
+            .setTitle(`🟢 ${username}'s System`)
+            .setDescription(`ID : ${id}`)
+            .addFields({
+                name: `🛜 Service`,
+                value: `🟢 Actif`,
+                inline: true
+            })
+            .addFields({
+                name: `🤖 Commands`,
+                value: `🟢 Actif`,
+                inline: true
+            })
+            .addFields({
+                name: `🏠 Nombre(s) de discord`,
+                value: `${client.guilds.cache.size}`,
+                inline: false
+            })
+            .addFields({
+                name: `💝 Mode Kawai`,
+                value: `🟢 Actif`,
+                inline: false
+            })
+            .setTimestamp()
+            .setThumbnail(`${client.user?.displayAvatarURL({ size: 1024 })}`)
+            .setFooter({
+                text: `${client.guilds.cache.get('1109634584225792091')?.name}`,
+                iconURL: `${client.guilds.cache.get('1109634584225792091')?.iconURL()}`
+            })
+        LogHook.send({
+            embeds: [Logs]
+        })
     }
 
 }
